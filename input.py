6ee00a7095a17eff214d95e34c2e79e9d15ea31d
Paul Sheridan <paul.sheridan@me.com>
2016-02-03 14:53:49 -0800

    adding all local python practice scripts

---
+++ input.py
@@ -0,0 +1,11 @@
+# This program says hello and asks for my name.
+print ('Hello world!')
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ' + myName)
+
+# This program says hello and asks for my name.
+print ('You there, what do you think you are doing here?')
+print('What is your name?')
+myName = input()
+print('You will be executed at once, ' + myName)
