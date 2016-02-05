417a860d90db71afbd94aa680eaa88baa6d10d5d
Paul Sheridan <paulsheridan@lappy.local>
2016-02-05 12:07:17 -0800

    adding strCon and removing extra stuff

---
+++ strCon.py
@@ -0,0 +1,20 @@
+words = [
+  "guitar",
+  "drums",
+  "bass",
+  "microphone",
+  "keyboard"]
+vowels = ['a', 'e', 'i', 'o', 'u']
+no_vowels = []
+
+def remove_vowels(each_word):
+  each_word = list(each_word)
+  for vowel in vowels:
+    while vowel in each_word:
+      each_word.remove(vowel)
+  each_word = ''.join(each_word)
+  return each_word
+
+for word in words:
+  no_vowels.append(remove_vowels(word))
+print(no_vowels)
